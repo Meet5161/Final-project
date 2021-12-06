b9a160b7c61a9186ec891cfaff6a647a724f15ac
Meet Chaudhari <mchaudhari8594@conestogac.on.ca>
2021-12-06 00:16:35 -0500

    Second commit in GitHub

--- first commit for final project.py
+++ first commit for final project.py
@@ -15,3 +15,24 @@ def printBoard(board):
     print(board[3] + " | " + board[4] + " | " + board[5])
     print("---------")
     print(board[6] + " | " + board[7] + " | " + board[8])
+
+# take player input
+def playerInput(board):
+    inp = int(input("Enter any number between 1-9: "))
+    if inp >= 1 and inp <= 9 and board[inp-1] == "-":
+        board[inp-1] = currentPlayer
+    else:
+        print("\nOops! Player is already in that spot! ")
+
+# check for win or tie 
+def checkHorizontle(board):
+    global winner
+    if board[0] == board[1] == board[2] and board[1] != "-":
+        winner = board[0]
+        return True
+    elif board[3] == board[4] == board[5] and board[3] != "-":
+        winner = board[3]
+        return True
+    elif board[6] == board[7] == board[8] and board[6] != "-":
+        winner = board[6]
+        return True
